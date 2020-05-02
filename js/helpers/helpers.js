22cdde648e4ab06c811bc949dd33a7c7bb55d755
João Rosa <jmrfrosa92@gmail.com>
2020-05-02 18:34:05 +0100

    Moved assignNote to Layer

--- js/helpers/helpers.js
+++ js/helpers/helpers.js
@@ -1,22 +1,4 @@
-import { pSilence, chordList } from './constants';
-
 const helpers = {
-  assignNote: (currLayer, currStep, minOct, maxOct, addSilence) => {
-    let newNote, newOctave;
-    if (helpers.getRandomInt(0, 100) <= pSilence && addSilence) {
-        console.log(`Assigned silence to ${currLayer.name} position ${currStep}`)
-        newNote = '', newOctave = '';
-        currLayer.notes[currStep] = newNote + newOctave;
-        currLayer.velMod[currStep] = helpers.getRandomInt(1, 2);
-    } else {
-        newNote = chordList.Cmin[helpers.getRandomInt(0, 2)][helpers.getRandomInt(0, 2)];
-        newOctave = helpers.getRandomInt(minOct, maxOct);
-        console.log(`Assigned ${newNote} ${newOctave} to ${currLayer.name} in position ${currStep}`);
-        currLayer.notes[currStep] = newNote + newOctave.toString();
-        currLayer.velMod[currStep] = helpers.getRandomInt(1, 2);
-    }
-  },
-
   getRandomInt: (min, max) => {
     min = Math.ceil(min);
     max = Math.floor(max);
