c1ee8f597f2477845ecfbf6b92f987889589a546
João Rosa <jmrfrosa92@gmail.com>
2020-05-02 18:34:20 +0100

    Fixed incorrect LedMatrix generation

--- js/classes/LedMatrix.js
+++ js/classes/LedMatrix.js
@@ -1,6 +1,18 @@
+import Led from './Led';
+import { STEPS_PER_LOOP } from '../helpers/constants';
+
 class LedMatrix {
   constructor(numLayers) {
-    this.leds = Array(numLayers).fill([]);
+    this.leds = Array.from({ length: numLayers }, (_, rowIndex) => {
+      const row = [];
+      const y = 150 + 130 * rowIndex;
+      for (let step = 0; step < STEPS_PER_LOOP; step++) {
+          const x = 150 + 120 * step;
+          const led = new Led(x, y, 120);
+          row.push(led);
+      }
+      return row;
+    });
   }
 }
 
