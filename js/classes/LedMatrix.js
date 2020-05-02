d22a6fd3ff775e1aeb236f2edd946317dd26873f
João Rosa <jmrfrosa92@gmail.com>
2020-05-02 18:53:38 +0100

    Documented new LedMatrix method

--- js/classes/LedMatrix.js
+++ js/classes/LedMatrix.js
@@ -3,13 +3,15 @@ import { STEPS_PER_LOOP } from '../helpers/constants';
 
 class LedMatrix {
   constructor(numLayers) {
+    // Generator function for LED array
     this.leds = Array.from({ length: numLayers }, (_, rowIndex) => {
+      // Each iteration of this function will fill the array
+      // with STEPS_PER_LOOP * Led objects
       const row = [];
       const y = 150 + 130 * rowIndex;
       for (let step = 0; step < STEPS_PER_LOOP; step++) {
-          const x = 150 + 120 * step;
-          const led = new Led(x, y, 120);
-          row.push(led);
+        const x = 150 + 120 * step;
+        row.push(new Led(x, y, 120));
       }
       return row;
     });
