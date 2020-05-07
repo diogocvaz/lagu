2fe0ef931e4751104871291a198967e1dc8f4481
João Rosa <jmrfrosa92@gmail.com>
2020-05-07 23:41:46 +0100

    Added webrtc connection capabilities

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ import {
   STEPS_PER_LOOP,
   NUMBER_OF_ROWS } from './js/helpers/constants';
   
+const streamDestination = Tone.context.createMediaStreamDestination();
+
 //initalize parameters for visuals
   
 let ledMatrix = new LedMatrix(NUMBER_OF_ROWS);
@@ -76,6 +78,8 @@ function generateLayers(octRange, releaseRange, key) {
     layer.led  = ledMatrix.leds[idx];
     layer.init();
     
+    Tone.connect(layer.gain, streamDestination);
+
     return layer;
   });
 }
@@ -109,3 +113,58 @@ function scheduleSequences(sequences) {
     Tone.Transport.scheduleRepeat(time => s.sequence.run(time), s.interval)
   });
 }
+
+const webSocket = new WebSocket(`ws://${window.location.host.split(':')[0]}:443/`);
+const peerConnection = new RTCPeerConnection();
+const streamTrack = streamDestination.stream.getAudioTracks()[0];
+peerConnection.addTrack(streamTrack, streamDestination.stream);
+
+peerConnection.onnegotiationneeded = e => {
+  console.log("Negotiation needed");
+  peerConnection.createOffer()
+    .then(offer => {
+      return peerConnection.setLocalDescription(offer);
+    })
+    .then(() => {
+      const sdpMsg = peerConnection.localDescription;
+      console.log("Sending message", sdpMsg);
+      // return localSck.send(JSON.stringify({ sdp: sdpMsg }));
+      return webSocket.send(JSON.stringify({ sdp: sdpMsg }));
+    })
+    .catch(error => console.log(error));
+}
+
+peerConnection.onicecandidate = e => {
+  console.log("On ICE candidate");
+  // return localSck.send(JSON.stringify({ candidate: e.candidate }));
+  return webSocket.send(JSON.stringify({ candidate: e.candidate }));
+}
+
+peerConnection.oniceconnectionstatechange = e => {
+  console.log("ICE state change: ")
+  console.log(peerConnection.iceConnectionState);
+}
+
+peerConnection.onopen = e => {
+  console.log("peerConnection opened");
+}
+
+webSocket.onmessage = e => {
+  console.log("Receiving local socket message", e);
+  const msg = JSON.parse(e.data);
+  if (msg.sdp) {
+    peerConnection.setRemoteDescription(new RTCSessionDescription(msg.sdp))
+    .then(() => {
+      return peerConnection.signalingState == "stable" || peerConnection.createAnswer()
+        .then(answer => peerConnection.setLocalDescription(answer))
+        .then(() => webSocket.send(JSON.stringify({ sdp: peerConnection.localDescription })));
+    })
+    .catch(error => console.log(error));
+  }
+  else if (msg.candidate) {
+    peerConnection.addIceCandidate(new RTCIceCandidate(msg.candidate))
+      .catch(error => console.log(error));
+  }
+}
+
+console.log("Initializing webrtc", peerConnection);
