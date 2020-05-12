a41f495016614e0fd77a2ff0cc658f086c73466d
diogocvaz <62757059+diogocvaz@users.noreply.github.com>
2020-05-12 13:12:34 +0200

    added intrument, noteLength and pSilence in layerDefaults

--- js/constants.js
+++ js/constants.js
@@ -6,6 +6,8 @@ export const winHeight = $(window).height() - WINDOW_PADDING;
 export const STEPS_PER_LOOP = 8;
 export const NUMBER_OF_ROWS = 3;
 
+export const BPM = 800;
+
 export const CHORD_LIST = {
     Cmaj: [
         ['C', 'E', 'G'], //Cmaj
@@ -27,19 +29,25 @@ export const LED_LIGHT_STATES = {
 
 export const layerDefaults = [{
         startOctave: 4,
-        startRelease: 1,
+        startRelease: 0.2,
         startPanner: -0.9,
-        interval: '3n',
+        interval: '1m',
         minOct: 3,
-        maxOct: 5
+        maxOct: 5,
+        instrument: 'grandpiano',
+        noteLength: '16n',
+        pSilence: 35
     },
     {
         startOctave: 3,
         startRelease: 2,
         startPanner: 0,
-        interval: '1n',
+        interval: '4m',
         minOct: 3,
-        maxOct: 4
+        maxOct: 4,
+        instrument: 'violin',
+        noteLength: '2m',
+        pSilence: 35
     },
     {
         startOctave: 4,
@@ -47,6 +55,9 @@ export const layerDefaults = [{
         startPanner: 0.9,
         interval: '2n',
         minOct: 3,
-        maxOct: 5
+        maxOct: 5,
+        instrument: 'grandpiano',
+        noteLength: '16n',
+        pSilence: 35
     }
 ];
