0aa71da22665d34f8152f810191452c5c2b132bf
diogocvaz <62757059+diogocvaz@users.noreply.github.com>
2020-06-22 23:25:37 +0200

    fixed Tone breakdown bug, added on screen log and layer death/rebirth with new fixed instrument

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ import {
     winWidth,
     winHeight,
     NUMBER_OF_ROWS,
-    CHORD_LIST,
+    SCALE_LIST as SCALE_LIST,
     LED_LIGHT_STATES,
     layerDefaults as lD,
     BPM
@@ -14,7 +14,8 @@ import grandpiano from "./samples/grandpiano/*.wav"
 import violin from "./samples/violin/*.wav"
 
 var dataWeather;
-var bckColor = 0;
+var backgroundColor = 0;
+var prevOnScreenItem, nextOnScreenItem;
 
 const fetchWeather = async () => {
     const res = await import('./js/fetchWeather.js');
@@ -25,22 +26,24 @@ const fetchWeather = async () => {
 
 window.setup = function () {
     fetchWeather();
+    Tone.Transport.start();
     setTimeout(() => {
-        console.log('start p5');
+        onScreenLog('Start p5.js');
+        startElapsedTime();
         createCanvas(winWidth, winHeight);
         scheduleSequence(arraySequences);
-        Tone.Transport.start();
         Tone.context.resume();
-        console.log('start Tonejs');
+        onScreenLog('Start Tone.js');
         console.log(dataWeather);
-        bckColor = (dataWeather.dayState[2] === 'day') ? 'orange' : 0;
-    }, 1000);
+        backgroundColor = (dataWeather.dayState[2] === 'day') ? 'orange' : 0;
+    }, 5000);
     // time to set Tone buffers before Tone.Transport.start()
 }
 
 window.draw = function () {
-    background(bckColor);
-    noStroke();
+    background(backgroundColor);
+    stroke(255, 255, 255);
+    //update leds
     let currNumSteps, currLed, currSilence;
     for (let rowIndex = 0; rowIndex < NUMBER_OF_ROWS; rowIndex++) {
         currNumSteps = arrayLayers[rowIndex].numOfSteps;
@@ -50,7 +53,7 @@ window.draw = function () {
             if (currLed.light == 1 && currSilence == 0 && currLed.counter < 5) {
                 currLed.changeFillColor(255, 255, 255);
                 currLed.counter += 1;
-            } else if (currLed.light == 2 && currLed.counter < 15) {
+            } else if (currLed.light == 2 && currLed.counter < 10) {
                 currLed.changeFillColor(255, 0, 0);
                 currLed.counter += 1;
             } else {
@@ -97,7 +100,6 @@ class Layer {
         this.decayMod = [];
         this.silenceMod = [];
         this.step = 0;
-
         this.interval = layerProp[layerNumber].interval;
 
         this.initOct = layerProp[layerNumber].startOctave;
@@ -108,29 +110,28 @@ class Layer {
         this.instrument = layerProp[layerNumber].instrument;
         this.noteLength = layerProp[layerNumber].noteLength;
         this.pSilence = layerProp[layerNumber].pSilence;
-        this.lifetime = layerProp[layerNumber].lifetime;
         this.numOfSteps = layerProp[layerNumber].numOfSteps;
         this.gainDamp = layerProp[layerNumber].gainDamp;
+
         // this.synth = this.makeSynth();
         this.sampler = this.makeSampler(this.instrument);
         this.panner = new Tone.Panner(this.pannerPosition);
         this.reverb = new Tone.Reverb(5);
         this.gain = new Tone.Gain(0.6);
-
         this.leds = [];
     }
     init() {
         let newNote = '';
         let newAttack, newVel, newDecay;
         for (let j = 0; j < this.numOfSteps; j++) {
-            newNote = CHORD_LIST.Cmin[getRandomNum(0, 2, 0)][getRandomNum(0, 2, 0)];
+            newNote = SCALE_LIST.Cmin[getRandomNum(0, 6, 0)];
             this.notes.push(newNote + this.initOct);
             newAttack = getRandomNum(1, 2, 0);
             this.attackMod.push(newAttack);
             this.releaseMod.push(this.maxRel);
             newVel = getRandomNum(0.8, 2, 1);
             this.velMod.push(newVel);
-            newDecay = getRandomNum(0.05, 0.2, 2);
+            newDecay = getRandomNum(0.1, 0.4, 2);
             this.decayMod.push(newDecay);
             this.silenceMod.push(0);
         }
@@ -185,7 +186,7 @@ class Layer {
         let posY = (this.layerNumber * 100) + 100;
         for (let step = 0; step < this.numOfSteps; step++) {
             let posX = 150 + 60 * step;
-            this.leds.push(new Led(posX, posY, 120));
+            this.leds.push(new Led(posX, posY, 100));
         }
     }
 }
@@ -208,53 +209,63 @@ class Sequence {
         this.noteLength = this.layer.noteLength;
         this.gainDamp = this.layer.gainDamp;
 
-        //note velocity damping
+        // note velocity damping
         if (this.vel > 0.0001) {
             this.silentStep = this.layer.silenceMod[this.cstep];
-            // trigger a note immediatly and trigger release after 1/16 measures
             if (this.silentStep == 1) {
+                // trigger a note with velocity 0 (silence)
                 this.layer.sampler.triggerAttackRelease(this.note, this.noteLength, time, 0);
             } else {
+                // trigger a note immediatly and trigger release after 1/16 measures
                 this.layer.sampler.triggerAttackRelease(this.note, this.noteLength, time, this.vel);
             }
-            // trigger visuals
+            // trigger led
             this.leds.light = LED_LIGHT_STATES.ON;
             this.leds.alpha = lerp(0, 255, this.vel / 2);
             // reduce velocity
             this.layer.velMod[this.cstep] = this.vel - this.layer.decayMod[this.cstep];
         } else {
+            // when a note's velocity reaches zero
             this.layer.velMod[this.cstep] = 0;
-            assignNote(this.layer, this.cstep, this.layer.minOct, this.layer.maxOct, 'y');
+            assignNote(this.layer, this.cstep, this.layer.minOct, this.layer.maxOct);
             this.note = this.layer.notes[this.cstep];
             this.vel = this.layer.velMod[this.cstep];
             this.silentStep = this.layer.silenceMod[this.cstep];
+            // play it right after it's assigned
             if (this.silentStep == 1) {
                 this.layer.sampler.triggerAttackRelease(this.note, this.noteLength, time, 0);
             } else {
                 this.layer.sampler.triggerAttackRelease(this.note, this.noteLength, time, this.vel);
             }
+            // trigger led
             this.leds.light = LED_LIGHT_STATES.NEW;
             this.leds.alpha = 255;
         }
 
-        //layer gain damping
-        if (this.cstep == 0) {
+        // layer gain damping
+        if (this.cstep == this.layer.notes.length - 1) {
             if (this.layer.gain.gain.input.value > 0.2) {
                 this.layer.gain.gain.input.value = this.gain - this.gainDamp;
-                // console.log(this.layer.gain.gain.input.value);
+                //console.log(this.layer.gain.gain.input.value);
             } else {
+                for (let i = 0; i < this.layer.velMod.length; i++) {
+                    this.layer.velMod[i] = 0;
+                }
+                // to execute when gain dies out
                 this.layer.gain.gain.input.value = 0.6;
+                // this.layer.instrument = 'grandpiano';
+                // this.layer.sampler = this.layer.makeSampler('grandpiano');
+                // this.layer.connectSampler();
+                // console.log(arrayLayers)
+                onScreenLog(`${this.layer.name} rebirth`);
                 console.log(`${this.layer.name} rebirth`);
-                //code to execute when gain dies out
             }
         }
-
         this.layer.step++;
     }
 }
 
 function generateLayers(lD) {
-    //layer number, initial octave, incitial release, panner
     return Array.from({
         length: NUMBER_OF_ROWS
     }, (_, idx) => {
@@ -268,7 +279,6 @@ function generateLayers(lD) {
 }
 
 function generateSequence(arrayLayers) {
-    //layer number, initial octave, incitial release, panner
     return Array.from({
         length: NUMBER_OF_ROWS
     }, (_, idx) => {
@@ -290,16 +300,20 @@ let arraySequences = generateSequence(arrayLayers);
 
 Tone.Transport.bpm.value = BPM;
 
-function assignNote(currLayer, currStep, minOct, maxOct, addSilence) {
+// aux functions
+
+function assignNote(currLayer, currStep, minOct, maxOct) {
     let newNote, newOctave;
-    if (getRandomNum(0, 100, 0) <= currLayer.pSilence && addSilence == 'y') {
-        console.log(`Assigned silence to ${currLayer.name} in position ${currStep}`);
+    if (getRandomNum(0, 100, 0) <= currLayer.pSilence) {
+        onScreenLog(`silence to ${currLayer.name} in position ${currStep}`);
+        console.log(`silence to ${currLayer.name} in position ${currStep}`);
         currLayer.silenceMod[currStep] = 1;
         currLayer.velMod[currStep] = getRandomNum(1, 2, 0);
     } else {
-        newNote = CHORD_LIST.Cmin[getRandomNum(0, 2, 0)][getRandomNum(0, 2, 0)];
+        newNote = SCALE_LIST.Cmin[getRandomNum(0, 6, 0)];
         newOctave = getRandomNum(minOct, maxOct, 0);
-        console.log(`Assigned ${newNote}${newOctave} to ${currLayer.name} in position ${currStep}`);
+        onScreenLog(`${newNote}${newOctave} to ${currLayer.name} in position ${currStep}`);
+        console.log(`${newNote}${newOctave} to ${currLayer.name} in position ${currStep}`);
         currLayer.notes[currStep] = newNote + newOctave.toString();
         currLayer.silenceMod[currStep] = 0;
         currLayer.velMod[currStep] = getRandomNum(1, 2, 0);
@@ -311,4 +325,32 @@ function getRandomNum(min, max, precision) {
     min = min * Math.pow(10, precision);
     max = max * Math.pow(10, precision);
     return (Math.floor(Math.random() * (max - min + 1)) + min) / Math.pow(10, precision);
+}
+
+
+
+function onScreenLog(textLog) {
+    for (let i = 0; i < 4; i++) {
+        prevOnScreenItem = 'entry' + i;
+        nextOnScreenItem = 'entry' + (i + 1);
+        document.getElementById(prevOnScreenItem).innerHTML = document.getElementById(nextOnScreenItem).innerHTML;
+    }
+    document.getElementById("entry4").innerHTML = textLog;
+}
+
+var initialTime = new Date();
+
+function startElapsedTime() {
+    var updatedTime = new Date();
+    var timeElapsed = convertTime(updatedTime.getTime() - initialTime.getTime());
+    document.getElementById('timeDisplay').innerHTML = "runtime: " + timeElapsed;
+    var t = setTimeout(startElapsedTime, 500); //update rate
+}
+
+function convertTime(unix_timestamp) {
+    let date = new Date(unix_timestamp);
+    let hours = date.getUTCHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    return hours + ":" + ((minutes < 10) ? "0" + minutes : minutes) + ":" + ((seconds < 10) ? "0" + seconds : seconds)
 }
