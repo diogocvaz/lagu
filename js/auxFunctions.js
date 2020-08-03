037c706cbfa248d3eb8fbb5e3a9e9bd0a3b28d11
diogocvaz <62757059+diogocvaz@users.noreply.github.com>
2020-08-04 00:34:29 +0200

    added idea for rand num generator (Box-Muller)

--- js/auxFunctions.js
+++ js/auxFunctions.js
@@ -44,3 +44,19 @@ function convertTime(unix_timestamp) {
     let seconds = date.getSeconds();
     return hours + ":" + ((minutes < 10) ? "0" + minutes : minutes) + ":" + ((seconds < 10) ? "0" + seconds : seconds)
 }
+
+// Box–Muller dist
+// https://stackoverflow.com/questions/25582882/javascript-math-random-normal-distribution-gaussian-bell-curve/36481059#36481059
+// function randn_bm(min, max, skew) {
+//     let u = 0, v = 0;
+//     while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+//     while(v === 0) v = Math.random();
+//     let num = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+
+//     num = num / 10.0 + 0.5; // Translate to 0 -> 1
+//     if (num > 1 || num < 0) num = randn_bm(min, max, skew); // resample between 0 and 1 if out of range
+//     num = Math.pow(num, skew); // Skew
+//     num *= max - min; // Stretch to fill range
+//     num += min; // offset to min
+//     return num;
+// }
