2186789d8a01f57f4f90611b771a97552e8dc619
diogocvaz <62757059+diogocvaz@users.noreply.github.com>
2020-08-29 12:55:34 +0200

    added new intrument parameters

--- js/supervisor.js
+++ js/supervisor.js
@@ -10,8 +10,10 @@ import * as auxf from './auxFunctions.js';
 var possibleSteps = [4, 8, 12, 16];
 var possibleStepsLarge = [8, 12, 16];
 var propertiesToSend;
-var possibleSynthsArray = ['grandpiano', 'analomagous', 'earth'];
-var possiblePadsArray = ['violin', 'milpad', 'alienpad'];
+var possibleSynthsArray = ['grandpiano', 'analomagous', 'earth', 'pyk', 'sazpluck', 'wiccle'];
+var possiblePadsArray = ['violin', 'milpad', 'alienpad', 'lightfogpad', 'emotpad', 'pingwoopad'];
+var possibleDamps = [0.01, 0.008, 0.006, 0.004, 0.002];
+
 
 export function instrumentDecider(layerNum) {
     if (layerNum == 0 || layerNum == 1) {
@@ -31,7 +33,7 @@ export function instrumentDecider(layerNum) {
                 startPanner: -0.8,
                 pSilence: auxf.getRandomNum(10, 50, 0),
                 numOfSteps: auxf.getRandomfromArray(possibleSteps),
-                gainDamp: 0.02, //0.002,
+                gainDamp: auxf.getRandomfromArray(possibleDamps), //0.002,
                 direction: -1
             }
         } else if (instrument == 'analomagous') {
@@ -49,7 +51,7 @@ export function instrumentDecider(layerNum) {
                 startPanner: 0.8,
                 pSilence: auxf.getRandomNum(10, 50, 0),
                 numOfSteps: auxf.getRandomfromArray(possibleSteps),
-                gainDamp: 0.02, //0.002,
+                gainDamp: auxf.getRandomfromArray(possibleDamps), //0.002,
                 direction: -1
             }
         } else if (instrument == 'earth') {
@@ -67,15 +69,69 @@ export function instrumentDecider(layerNum) {
                 startPanner: 0.8,
                 pSilence: auxf.getRandomNum(10, 50, 0),
                 numOfSteps: auxf.getRandomfromArray(possibleSteps),
-                gainDamp: 0.02, //0.002,
+                gainDamp: auxf.getRandomfromArray(possibleDamps), //0.002,
+                direction: -1
+            }
+        } else if (instrument == 'pyk') {
+            propertiesToSend = {
+                instrument: 'pyk',
+                startOctave: auxf.getRandomNum(2, 3, 0),
+                minOct: 2,
+                maxOct: 3,
+                maxRelease: auxf.getRandomNum(1, 10, 0),
+                noteLength: auxf.getRandomNum(1, 8, 0) + 'n',
+                interval: auxf.getRandomNum(2, 4, 0) + 'n',
+                mainGain: 1,
+                maxGain: 1,
+                reverbValue: auxf.getRandomNum(0.1, 1, 1),
+                startPanner: 0.8,
+                pSilence: auxf.getRandomNum(10, 50, 0),
+                numOfSteps: auxf.getRandomfromArray(possibleSteps),
+                gainDamp: auxf.getRandomfromArray(possibleDamps), //0.002,
+                direction: -1
+            }
+        } else if (instrument == 'sazpluck') {
+            propertiesToSend = {
+                instrument: 'sazpluck',
+                startOctave: auxf.getRandomNum(2, 4, 0),
+                minOct: 2,
+                maxOct: 4,
+                maxRelease: auxf.getRandomNum(1, 10, 0),
+                noteLength: auxf.getRandomNum(1, 8, 0) + 'n',
+                interval: auxf.getRandomNum(2, 4, 0) + 'n',
+                mainGain: 1,
+                maxGain: 1,
+                reverbValue: auxf.getRandomNum(0.1, 1, 1),
+                startPanner: 0.8,
+                pSilence: auxf.getRandomNum(10, 50, 0),
+                numOfSteps: auxf.getRandomfromArray(possibleSteps),
+                gainDamp: auxf.getRandomfromArray(possibleDamps), //0.002,
+                direction: -1
+            }
+        } else if (instrument == 'wiccle') {
+            propertiesToSend = {
+                instrument: 'wiccle',
+                startOctave: auxf.getRandomNum(3, 4, 0),
+                minOct: 3,
+                maxOct: 4,
+                maxRelease: auxf.getRandomNum(1, 10, 0),
+                noteLength: auxf.getRandomNum(1, 8, 0) + 'n',
+                interval: auxf.getRandomNum(2, 4, 0) + 'n',
+                mainGain: 0.8,
+                maxGain: 0.8,
+                reverbValue: auxf.getRandomNum(0.1, 1, 1),
+                startPanner: 0.8,
+                pSilence: auxf.getRandomNum(10, 50, 0),
+                numOfSteps: auxf.getRandomfromArray(possibleSteps),
+                gainDamp: auxf.getRandomfromArray(possibleDamps), //0.002,
                 direction: -1
             }
         }
     } else if (layerNum == 2) {
-        var instrument = auxf.getRandomfromArray(['dirtybass']);
-        if (instrument == 'dirtybass') {
+        var instrument = auxf.getRandomfromArray(['deepbass']);
+        if (instrument == 'deepbass') {
             propertiesToSend = {
-                instrument: 'dirtybass',
+                instrument: 'deepbass',
                 startOctave: 1,
                 minOct: 1,
                 maxOct: 1,
@@ -109,15 +165,15 @@ export function instrumentDecider(layerNum) {
                 startPanner: 0.3,
                 pSilence: auxf.getRandomNum(20, 60, 0),
                 numOfSteps: auxf.getRandomfromArray(possibleStepsLarge),
-                gainDamp: 0.02, //0.002,
+                gainDamp: 0.005, //0.002,
                 direction: -1
             }
         } else if (instrument == 'milpad') {
             propertiesToSend = {
                 instrument: 'milpad',
-                startOctave: auxf.getRandomNum(3, 4, 0),
+                startOctave: auxf.getRandomNum(2, 3, 0),
                 minOct: 2,
-                maxOct: 4,
+                maxOct: 3,
                 maxRelease: 10,
                 noteLength: auxf.getRandomNum(1, 2, 0) + 'n',
                 interval: auxf.getRandomNum(2, 4, 0) + 'n',
@@ -127,14 +183,13 @@ export function instrumentDecider(layerNum) {
                 startPanner: 0.3,
                 pSilence: auxf.getRandomNum(20, 60, 0),
                 numOfSteps: auxf.getRandomfromArray(possibleStepsLarge),
-                gainDamp: 0.02, //0.002,
+                gainDamp: 0.005, //0.002,
                 direction: -1
             }
-        }
-        if (instrument == 'alienpad') {
+        } else if (instrument == 'alienpad') {
             propertiesToSend = {
                 instrument: 'alienpad',
-                startOctave: auxf.getRandomNum(3, 4, 0),
+                startOctave: auxf.getRandomNum(2, 4, 0),
                 minOct: 2,
                 maxOct: 4,
                 maxRelease: 10,
@@ -146,7 +201,61 @@ export function instrumentDecider(layerNum) {
                 startPanner: 0.3,
                 pSilence: auxf.getRandomNum(20, 60, 0),
                 numOfSteps: auxf.getRandomfromArray(possibleStepsLarge),
-                gainDamp: 0.02, //0.002,
+                gainDamp: 0.005, //0.002,
+                direction: -1
+            }
+        } else if (instrument == 'lightfogpad') {
+            propertiesToSend = {
+                instrument: 'lightfogpad',
+                startOctave: auxf.getRandomNum(1, 2, 0),
+                minOct: 1,
+                maxOct: 2,
+                maxRelease: 10,
+                noteLength: auxf.getRandomNum(2, 4, 0) + 'm',
+                interval: auxf.getRandomNum(1, 2, 0) + 'n',
+                mainGain: 0.5,
+                maxGain: 0.5,
+                reverbValue: 1,
+                startPanner: 0.3,
+                pSilence: auxf.getRandomNum(20, 60, 0),
+                numOfSteps: auxf.getRandomfromArray(possibleStepsLarge),
+                gainDamp: 0.005, //0.002,
+                direction: -1
+            }
+        } else if (instrument == 'emotpad') {
+            propertiesToSend = {
+                instrument: 'emotpad',
+                startOctave: auxf.getRandomNum(2, 3, 0),
+                minOct: 2,
+                maxOct: 3,
+                maxRelease: 10,
+                noteLength: auxf.getRandomNum(1, 2, 0) + 'm',
+                interval: '1n',
+                mainGain: 0.6,
+                maxGain: 0.6,
+                reverbValue: 1,
+                startPanner: 0.3,
+                pSilence: auxf.getRandomNum(20, 60, 0),
+                numOfSteps: auxf.getRandomfromArray(possibleStepsLarge),
+                gainDamp: 0.005, //0.002,
+                direction: -1
+            }
+        } else if (instrument == 'pingwoopad') {
+            propertiesToSend = {
+                instrument: 'pingwoopad',
+                startOctave: auxf.getRandomNum(2, 4, 0),
+                minOct: 2,
+                maxOct: 4,
+                maxRelease: 10,
+                noteLength: auxf.getRandomNum(2, 4, 0) + 'm',
+                interval: auxf.getRandomNum(1, 2, 0) + 'n',
+                mainGain: 0.6,
+                maxGain: 0.6,
+                reverbValue: 1,
+                startPanner: 0.3,
+                pSilence: auxf.getRandomNum(20, 60, 0),
+                numOfSteps: auxf.getRandomfromArray(possibleStepsLarge),
+                gainDamp: 0.005, //0.002,
                 direction: -1
             }
         }
